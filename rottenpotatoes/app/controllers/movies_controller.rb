aedd1c0aeff9e554b9d3e9dced3843885ffb9630
sunayna-ray <sunayna0501@gmail.com>
2021-10-15 00:24:49 +0000

    search movies by director

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -30,6 +30,17 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
+  
+   def similar
+    @movie_title=params[:movie_title]
+    if Movie.find_by(title: @movie_title).director
+      @sim_movies=Movie.similar_director_movies(Movie.find_by(title: @movie_title).director)
+    else
+      flash[:notice] = "No director info found for #{Movie.find_by(title: @movie_title).title}"
+      redirect_to movies_path
+    end
+  end
+  
 
   def destroy
     @movie = Movie.find(params[:id])
--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+    def self.similar_director_movies movie_director
+        return Movie.where(director: movie_director).all
+    end
 end
