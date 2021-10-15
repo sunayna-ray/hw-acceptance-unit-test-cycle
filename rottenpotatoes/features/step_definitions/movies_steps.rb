a971a6e690084b86178987e90ebb817ede7f2c3e
sunayna-ray <sunayna0501@gmail.com>
2021-10-15 03:53:14 +0000

    Adding HW3 files

--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -5,15 +5,32 @@ Given /the following movies exist/ do |movies_table|
   end
 end
 
+Then /(.*) seed movies should exist/ do | n_seeds |
+  expect(Movie.count).to eq n_seeds.to_i
+end
+
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
   expect(page.body.index(e1) < page.body.index(e2))
 end
 
+# When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+#   rating_list.split(', ').each do |rating|
+#     step %{I #{uncheck.nil? ? '' : 'un'}check "ratings_#{rating}"}
+#   end
+# end
+
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
   rating_list.split(', ').each do |rating|
-    step %{I #{uncheck.nil? ? '' : 'un'}check "ratings_#{rating}"}
+    if uncheck
+      uncheck("ratings[#{rating}]")
+    else
+      check("ratings[#{rating}]")
+    end
   end
 end
 
